a2ff62975574a8cb21eda7523ccc8ac8e159ca6d
sunny <596541136@qq.com>
2016-10-13 21:24:15 +0800

    canvas

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
-var canvasWidth=800;
-var canvasHeight=600;
+var canvasWidth=window.innerWidth;
+var canvasHeight=window.innerHeight;
 
 
 var canvas=document.getElementById('canvas');
+var can_div=document.getElementById('can-div');
+var can_img=can_div.getElementsByClassName("img-blur")[0];
 var ctx=canvas.getContext('2d');
 canvas.width=canvasWidth;
 canvas.height=canvasHeight;
 
 var img=new Image();
 var radius=50;
+var topMargin=0;
+var leftMargin=0;
 var clipImg={x:Math.random()*(canvas.width-2*radius)+radius, y:Math.random()*(canvas.height-2*radius)+radius, r:radius};
 img.src='img/1.jpg';
 img.onload=function(){
+	can_div.style.width=canvasWidth+'px';
+	can_div.style.height=canvasHeight+'px';
+	
+	can_img.style.width=img.width+'px';
+	can_img.style.height=img.height+'px';
+	
+	topMargin=(img.height-canvas.height)/2;
+	leftMargin=(img.width-canvas.width)/2;
+	
+	can_img.style.left=String(-leftMargin)+'px';
+	can_img.style.top=String(-topMargin)+'px';
+	
 	iniImg();
+	console.log(img.height+' '+canvas.height+' '+topMargin+' '+can_img.offsetTop);
+	console.log(img.width+' '+canvas.width+' '+leftMargin+' '+can_img.offsetLeft);
+	
+	
 }
 function iniImg(){
-	clipImg={x:Math.random()*(canvas.width-2*radius)+radius, y:Math.random()*(canvas.height-2*radius)+radius, r:radius};
+	//当画布比图片大的时候多出来的部分
+	var theLeft=leftMargin<0?-leftMargin:0;
+	var theTop=topMargin<0?-topMargin:0;
+	clipImg={x:Math.random()*(canvas.width-2*radius-2*theLeft)+radius+theLeft,
+			 y:Math.random()*(canvas.height-2*radius-2*theTop)+radius+theTop, r:radius};
 	draw(img,clipImg);
 }
 //剪辑区域
@@ -29,10 +53,14 @@ function draw(img,clipImg){
 	
 	ctx.save();
 	setClipImg(clipImg);
-	ctx.drawImage(img,0,0);
+	ctx.drawImage(img,Math.max(leftMargin,0),Math.max(topMargin,0),
+					Math.min(canvas.width,img.width),Math.min(canvas.height,img.height),
+					leftMargin<0?-leftMargin:0,topMargin<0?-topMargin:0,
+					Math.min(canvas.width,img.width),Math.min(canvas.height,img.height));
 	ctx.restore();
 }
 function reset(){
+	
 	if(clipImg.r<=50){
 		iniImg();
 	}else if (clipImg.r>=1000) {
@@ -46,8 +74,6 @@ function reset(){
 	}
 	
 	
-	
-	
 }
 function show(){
 	var timer=setInterval(function(){
@@ -57,6 +83,7 @@ function show(){
 			clearInterval(timer);
 		}
 	},30);
+	
 }
 
 
